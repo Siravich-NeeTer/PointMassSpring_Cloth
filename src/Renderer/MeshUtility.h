6b081471ceb8911f8d55389030f66c1630d03c69
Siravich-NeeTer <Siravich-NeeTer@users.noreply.github.com>
2024-06-21 06:15:21 +0700

    [Hot Fix] Fixed Missing Files

--- src/Renderer/MeshUtility.h
+++ src/Renderer/MeshUtility.h
@@ -10,6 +10,15 @@
 #include <iostream>
 #include <vector>
 
+struct V3N3T2
+{
+	glm::vec3 vertices;
+	glm::vec3 normal;
+	glm::vec2 textureCoords;
+};
+
 void GenerateCubeBuffer(const VAO& vertexArray, const VBO& vertexBuffer, const EBO& elementBuffer, std::vector<GLuint>* indices = nullptr);
 
 void GenerateCubeBufferWithTexture(const VAO& vertexArray, const VBO& vertexBuffer, const EBO& elementBuffer, std::vector<GLuint>* indices = nullptr);
+
+void GenerateCubeBufferWithTextureNormal(const VAO& vertexArray, const VBO& vertexBuffer, const EBO& elementBuffer, std::vector<GLuint>* indices = nullptr);
